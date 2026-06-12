3e470fc9431311bbc22ba6d224560e26eff5d16e
myomarlar <myomarlarsolutions@gmail.com>
2026-06-12 07:25:14 +0630

    dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,1 +1,11 @@
 /** @format */
+export default function DashboardLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div>
+      {children}
+      <h1>Dashboard Layout</h1>
+    </div>
+  );
+}
